d7eaf2d2053ab59e74785a46448578c51b269805
SpinnyFinny <finn.hiltner@hotmail.com>
2017-11-09 20:30:02 -0800

    email sends on order with order id in subject line

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,7 @@
 class UserMailer < ApplicationMailer
+  def order_email(order)
+    # @user = user
+    byebug
+    mail(to: order.email, subject: order.id)
+  end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,4 +40,24 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   config.web_console.whitelisted_ips = ['10.0.2.0/24']
+
+  config.action_mailer.delivery_method = :sendmail
+  # Defaults to:
+  # config.action_mailer.sendmail_settings = {
+  #   location: '/usr/sbin/sendmail',
+  #   arguments: '-i'
+  # }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'lighthousetestaddress@gmail.com'}
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  # domain:               'example.com',
+  user_name:            'lighthousetestaddress@gmail.com',
+  password:             'joel@joel.joel',
+  authentication:       'plain',
+  enable_starttls_auto: true  }
 end
