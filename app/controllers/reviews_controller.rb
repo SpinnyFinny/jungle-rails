a0782b1f0b2dcb320e5b893c788801b1a5f4d138
SpinnyFinny <finn.hiltner@hotmail.com>
2017-11-11 15:09:27 -0800

    new reviews are now added to db

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,28 @@
 class ReviewsController < ApplicationController
   def create
-    redirect_to :back
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    @review.user_id = 1
+    
+    if @review.save
+      redirect_to :back          
+    else
+      redirect_to :back       
+    end
   end
 
   def destroy
 
   end
 end
+
+private
+
+  def review_params
+    params.require(:review).permit(
+      :description,
+      :rating,
+      :product_id,
+      :user_id
+    )
+  end
