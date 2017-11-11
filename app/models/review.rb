4d638c259177c001b8e0d0594c5d7c2d71f88a88
SpinnyFinny <finn.hiltner@hotmail.com>
2017-11-10 16:46:21 -0800

    rendered review on product show page

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,6 @@ class Review < ActiveRecord::Base
   validates_presence_of :user_id
   validates_presence_of :description
   validates_presence_of :rating
+  belongs_to :user
+  belongs_to :product
 end
